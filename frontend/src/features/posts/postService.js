25c964b2fcfb1614ca7368625b1b3072bb154b93
Yuandjom <ziyangjohn@gmail.com>
2022-12-26 14:34:10 +0800

    Sign in and Login API call

--- frontend/src/features/posts/postService.js
+++ frontend/src/features/posts/postService.js
@@ -1,13 +1,34 @@
 import axios from "axios";
 //this is the root URL
-const API_URL = "/posts/";
+const API_URL = "/posts/"; //note that the default URL is in index.js
 
+//Get all posts
 export const getAllPosts = async () => {
-  const res = await axios.get("http://localhost:5000/posts");
+  const res = await axios.get(API_URL);
   if (res.status !== 200) {
     return console.log("Some Error Occurred");
   }
 
   const data = res.data;
   return data;
+};
+
+/**
+ * Sign up and Login API function to sendAuthRequest to the backend
+ */
+export const sendAuthRequest = async (signup, data) => {
+  //note that we are sending the data to the backend
+  const res = await axios
+    .post(`/user/${signup ? "signup" : "login"}/`, {
+      name: data.name ? data.name : "",
+      email: data.email,
+      password: data.password,
+    })
+    .catch((err) => console.log(err));
+
+  if (res.status !== 200 && res.status !== 201) {
+    return console.log("Unable to Autheticate");
+  }
+  const resData = await res.data;
+  return resData;
 };
--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -1,9 +1,95 @@
 import React from 'react'
+import {Box, FormLabel, TextField, Typography, Button} from "@mui/material"
+import { useState } from 'react'
+import { sendAuthRequest } from '../features/posts/postService'
 function Auth() {
+  
+  const [isSignup, setisSignup] = useState(false)
+  //note that the values of the inputs are in the values of the form 
+  const [inputs, setInputs] = useState({
+    name: "", 
+    email:"", 
+    password:""
+  })
+  
+  const handleChange = (e) => {
+    //we need to save the prev state also
+    setInputs((prevState) => ({
+      ...prevState, 
+      [e.target.name] : e.target.value, 
+    }))
+  }
+  const handleSubmit = (e) => {
+    //need this to prevent the html page from switching to the default state
+    e.preventDefault()
+    console.log(inputs)
+    if(isSignup){
+      //we are sending the inputs object into the data(parameters of sendAuthRequest in postService)
+      sendAuthRequest(true, inputs).then((data) => console.log(data)).catch(err=> console.log(err))
+    }
+    else{
+      sendAuthRequest(false, inputs).then((data) => console.log(data)).catch(err=>console.log(err))
+    }
+  }
+  
   return (
-    <div>
-        Auth
-    </div>
+    <Box 
+      width="40%" 
+      borderRadius={10}
+      boxShadow={"5px 5px 10px #ccc"}
+      margin = "auto"
+      marginTop={10}
+    > 
+      <form onSubmit={handleSubmit}>
+        <Box 
+          display='flex' 
+          flexDirection={'column'}
+          width="60%"
+          padding={5}
+          margin="auto"
+        >
+          <Typography padding={1} variant='h4' textAlign="center">
+            {isSignup ? "Signup": "Login"}
+          </Typography>
+          {isSignup && (
+            <>
+            <FormLabel>Name</FormLabel>
+            <TextField 
+              onChange={handleChange}
+              value={inputs.name} 
+              name="name" 
+              required 
+              margin='normal'
+            />
+            </>
+          )}
+          <FormLabel>Email</FormLabel>
+          <TextField 
+            onChange={handleChange}
+            value={inputs.email} 
+            name="email" 
+            type="email"
+            required 
+            margin='normal'
+          />
+          <FormLabel>Password</FormLabel>
+          <TextField 
+            onChange={handleChange}
+            value={inputs.password} 
+            name="password" 
+            type="password"
+            required 
+            margin='normal'
+          />
+          <Button sx={{mt: 2, borderRadius: 10}} type="submit" variant="contained">
+            {isSignup ? "Signup": "Login"}
+          </Button>
+          <Button onClick={() => setisSignup(!isSignup)} sx={{mt: 2, borderRadius: 10}}type="submit" variant="outlined">
+            {isSignup? "Change to Login": "Change to Signup"}
+          </Button>
+        </Box>
+      </form>
+    </Box>
   )
 }
 
